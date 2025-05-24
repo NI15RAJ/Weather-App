647bfef92a3365ff12213f3b85a7914c492ee9ca
NI15RAJ <nirajmaheshwaribtechcse22-26@liet.in>
2025-05-24 23:19:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useWeather } from './context/weather';
+import Input from './components/input';
+import Card from './components/card';
+import Button from './components/button';
+import './App.css';
+
+function App() {
+  const weather = useWeather();
+
+  useEffect(() => {
+    weather.fetchCurrentUserLocationData(); 
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Input />
+      <Button onClick={weather.fetchData} value="Search" />
+      <Card />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ weather.jsx
@@ -0,0 +1,45 @@
+import { createContext,
+   useContext, 
+   useState } from "react";
+import {
+  getWeatherDataForCity,
+  getWeatherDataForLocation,
+} from "../api";
+
+const WeatherContext = createContext(null);
+
+export const useWeather = () => useContext(WeatherContext);
+
+export const WeatherProvider = ({ children }) => {
+  const [searchCity, setSearchCity] = useState("");
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    if (!searchCity) return;
+    const response = await getWeatherDataForCity(searchCity);
+    setData(response);
+  };
+
+  const fetchCurrentUserLocationData = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeatherDataForLocation(
+        position.coords.latitude,
+        position.coords.longitude
+      ).then((res) => setData(res));
+    });
+  };
+
+  return (
+    <WeatherContext.Provider
+      value={{
+        searchCity,
+        setSearchCity,
+        data,
+        fetchData,
+        fetchCurrentUserLocationData,
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+};
