dd0c3572630d700217648c437765c446e3f8f130
NIRAJ MAHESHWARI <nirajmaheshwaribtechcse22-26@liet.in>
2025-07-10 09:28:20 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,7 @@ const baseURL =
   "https://api.weatherapi.com/v1/current.json?key=f862b470fcbe4103aa5195553250404";
 
 export const getWeatherDataForCity = async (city) => {
+  
   const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
   return await response.json();
 };
