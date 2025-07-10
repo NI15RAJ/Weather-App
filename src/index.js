e82344b7643cb1b6acaef02c48d098f49794d64b
NIRAJ MAHESHWARI <nirajmaheshwaribtechcse22-26@liet.in>
2025-07-10 09:25:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WeatherProvider>
-    
+
+  
       <App />
-    
+
+  
     </WeatherProvider>
   </React.StrictMode>
 );
